f6bc8e102487287861ad0b1c3c3fce47c039156d
matej <matej@DESKTOP-7T5V13I>
2025-12-23 14:42:04 +0100

    added a few functions

--- server/main.go
+++ server/main.go
@@ -1,34 +1,90 @@
 package main
 
 import (
+	"bufio"
 	"context"
 	"fmt"
 	"log"
 	"net"
+	"os"
 	"sync"
 
 	pb "razpravljalnica"
 
 	"google.golang.org/grpc"
 	"google.golang.org/protobuf/types/known/emptypb"
+	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 // implementation of MessageBoard
 type Server struct {
 	pb.UnimplementedMessageBoardServer
-	mutex      sync.RWMutex
+	mutex sync.RWMutex
+
 	users      map[int64]*pb.User
 	nextUserID int64
+
+	topics      map[int64]*pb.Topic
+	topicChat   map[int64][]*pb.Message
+	nextTopicID int64
+
+	messages      map[int64]*pb.Message
+	nextMessageID int64
 }
 
 func NewServer() *Server {
-	return &Server{
+	s := &Server{
 		users:      make(map[int64]*pb.User),
 		nextUserID: 1,
+
+		topics:      make(map[int64]*pb.Topic),
+		topicChat:   make(map[int64][]*pb.Message),
+		nextTopicID: 1,
+
+		messages:      make(map[int64]*pb.Message),
+		nextMessageID: 1,
+	}
+
+	// press enter in the server terminal to print the state
+	go func() {
+		reader := bufio.NewReader(os.Stdin)
+		for {
+			_, err := reader.ReadString('\n')
+			if err != nil {
+				return
+			}
+			s.PrintState()
+		}
+	}()
+
+	return s
+}
+
+// prints all the information stored in the server for testing
+func (s *Server) PrintState() {
+	s.mutex.RLock()
+	defer s.mutex.RUnlock()
+
+	fmt.Println("users:")
+	for id, user := range s.users {
+		fmt.Printf("ID = %d, Name = %s\n", id, user.Name)
+	}
+
+	fmt.Println("\ntopics:")
+	for id, topic := range s.topics {
+		fmt.Printf("ID = %d, Name = %s\n", id, topic.Name)
+	}
+
+	fmt.Println("\nmessages:")
+	for topicID, Name := range s.topics {
+		fmt.Printf("Topic ID = %d, Name = %s\n", topicID, Name.Name)
+		for _, msg := range s.topicChat[topicID] {
+			fmt.Printf("\tUserID = %d, Text = %s\n", msg.UserId, msg.Text)
+		}
 	}
 }
 
-// createUser creates a new user and assigns it an ID
+// creates a new user and assigns it an ID
 func (s *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb.User, error) {
 	s.mutex.Lock()
 	defer s.mutex.Unlock()
@@ -44,24 +100,100 @@ func (s *Server) CreateUser(ctx context.Context, req *pb.CreateUserRequest) (*pb
 	return user, nil
 }
 
+// creates a new topic and assigns it an ID
 func (s *Server) CreateTopic(ctx context.Context, req *pb.CreateTopicRequest) (*pb.Topic, error) {
-	return nil, fmt.Errorf("not implemented")
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	topic := &pb.Topic{
+		Id:   s.nextTopicID,
+		Name: req.Name,
+	}
+	s.topics[topic.Id] = topic
+	s.nextTopicID++
+
+	log.Printf("created topic: ID=%d, name=%s", topic.Id, topic.Name)
+	return topic, nil
 }
 
+// adds a new message to a topic
 func (s *Server) PostMessage(ctx context.Context, req *pb.PostMessageRequest) (*pb.Message, error) {
-	return nil, fmt.Errorf("not implemented")
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.topics[req.TopicId] == nil {
+		return nil, fmt.Errorf("topic with ID %d does not exist", req.TopicId)
+	}
+
+	message := &pb.Message{
+		Id:        s.nextMessageID,
+		TopicId:   req.TopicId,
+		UserId:    req.UserId,
+		Text:      req.Text,
+		CreatedAt: timestamppb.Now(),
+		Likes:     0,
+	}
+
+	s.topicChat[req.TopicId] = append(s.topicChat[req.TopicId], message)
+	s.messages[s.nextMessageID] = message
+	s.nextMessageID++
+
+	log.Printf("posted message: topicID=%d, userID=%d, text=%s", req.TopicId, req.UserId, req.Text)
+	return message, nil
 }
 
+// updates the text of an existing message
 func (s *Server) UpdateMessage(ctx context.Context, req *pb.UpdateMessageRequest) (*pb.Message, error) {
-	return nil, fmt.Errorf("not implemented")
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.messages[req.MessageId] == nil {
+		return nil, fmt.Errorf("message with ID %d does not exist", req.MessageId)
+	}
+
+	if s.messages[req.MessageId].UserId != req.UserId {
+		return nil, fmt.Errorf("user with ID %d is not the owner of message with ID %d", req.UserId, req.MessageId)
+	}
+
+	s.messages[req.MessageId].Text = req.Text
+
+	log.Printf("updated message: topicID=%d, userID=%d, text=%s", req.TopicId, req.UserId, req.Text)
+	return s.messages[req.MessageId], nil
 }
 
+// deletes an existing message
 func (s *Server) DeleteMessage(ctx context.Context, req *pb.DeleteMessageRequest) (*emptypb.Empty, error) {
-	return nil, fmt.Errorf("not implemented")
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.messages[req.MessageId] == nil {
+		return nil, fmt.Errorf("message with ID %d does not exist", req.MessageId)
+	}
+
+	if s.messages[req.MessageId].UserId != req.UserId {
+		return nil, fmt.Errorf("user with ID %d is not the owner of message with ID %d", req.UserId, req.MessageId)
+	}
+
+	delete(s.messages, req.MessageId)
+
+	log.Printf("deleted message: messageID=%d", req.MessageId)
+	return &emptypb.Empty{}, nil
+
 }
 
+// increments the like count of an existing message
 func (s *Server) LikeMessage(ctx context.Context, req *pb.LikeMessageRequest) (*pb.Message, error) {
-	return nil, fmt.Errorf("not implemented")
+	s.mutex.Lock()
+	defer s.mutex.Unlock()
+
+	if s.messages[req.MessageId] == nil {
+		return nil, fmt.Errorf("message with ID %d does not exist", req.MessageId)
+	}
+
+	s.messages[req.MessageId].Likes++
+
+	log.Printf("liked message: messageID=%d, totalLikes=%d", req.MessageId, s.messages[req.MessageId].Likes)
+	return s.messages[req.MessageId], nil
 }
 
 func (s *Server) GetSubscriptionNode(ctx context.Context, req *pb.SubscriptionNodeRequest) (*pb.SubscriptionNodeResponse, error) {
@@ -89,8 +221,10 @@ func main() {
 	// create a new grpc server
 	grpcServer := grpc.NewServer()
 
+	s := NewServer()
+
 	// register MessageBoard service
-	pb.RegisterMessageBoardServer(grpcServer, NewServer())
+	pb.RegisterMessageBoardServer(grpcServer, s)
 
 	log.Println("server is running on port 50051...")
 
